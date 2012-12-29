b9ed8de83fdd09e7f5c24897e074bed0eaca02f8
Trey Tucker <trey@treytucker.com>
2012-12-29 14:59:51 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpoutApp::Application.config.secret_token = '458a8a21df5712910f6e877e14a6017d71037acc3d8b4da1e0410b78d700de3961b0fa3af2bcce78577c247c16e3b7c7c5ef493a0778041adc14ab9e0f59ecb7'
